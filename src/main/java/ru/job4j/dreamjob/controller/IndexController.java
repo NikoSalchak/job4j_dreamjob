1d8c47e9016873909055d964c60c5dfc1d646fea
NikoSalchak <nikosalchak@gmail.com>
2025-09-02 20:31:53 +0500

    1. Генератор HTML [#504838]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping("/index")
     public String getIndex() {
-        return "index";
+        return "helloIndex";
     }
 }
