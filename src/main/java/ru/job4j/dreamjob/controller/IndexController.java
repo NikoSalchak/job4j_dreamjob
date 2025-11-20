f34b28da7a35733fdfc6420af225a185089fead6
NikoSalchak <nikosalchak@gmail.com>
2025-11-20 17:29:12 +0500

    3. Filter [#504865]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.dreamjob.model.User;
 
 @Controller
 @SuppressWarnings("unused")
@@ -12,12 +11,6 @@ public class IndexController {
 
     @GetMapping({"/", "/index"})
     public String getIndex(Model model, HttpSession session) {
-        var user = (User) session.getAttribute("user");
-        if (user == null) {
-            user = new User();
-            user.setName("Гость");
-        }
-        model.addAttribute("user", user);
         return "index";
     }
 }
